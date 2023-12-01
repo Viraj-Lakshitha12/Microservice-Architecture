280b4ed123b8106b39413512c6d1713c999153d9
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-01 09:04:19 +0530

    updated

--- item-service/src/main/java/lk/ijse/gdse/itemservice/api/ItemController.java
+++ item-service/src/main/java/lk/ijse/gdse/itemservice/api/ItemController.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.itemservice.api;
 
 import lk.ijse.gdse.itemservice.dto.ItemDTO;
+import lk.ijse.gdse.itemservice.dto.User;
 import lk.ijse.gdse.itemservice.entity.Item;
 import lk.ijse.gdse.itemservice.service.ItemService;
 import lk.ijse.gdse.itemservice.util.DataTypeConversion;
@@ -34,11 +35,9 @@ public class ItemController {
     }
 
     @PostMapping("/saveItem1")
-    public ItemDTO saveData1(@RequestBody ItemDTO itemDTO){
+    public User saveData1(@RequestBody User user){
 //        itemService.saveData(dataTypeConversion.getItemEntity(itemDTO));
-        ItemDTO i  = restTemplate.postForObject("http://localhost:8081/api/v1/user/saveUser1", itemDTO, ItemDTO.class);
-        System.out.println("item dto : "+i);
-        return i;
+        return restTemplate.postForObject("http://localhost:8081/api/v1/user/saveUser1", user, User.class);
     }
 
 }
---
+++ item-service/src/main/java/lk/ijse/gdse/itemservice/dto/User.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.itemservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String address;
+    private String salary;
+}
